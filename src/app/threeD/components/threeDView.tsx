23b1d2069ace2864e1ed671446d3ed4e7c74a0d1
MaxRonnow <max.hk.ronnow@gmail.com>
2025-02-03 17:00:13 +0200

    Added buttons and description to 3d page

--- src/app/threeD/components/threeDView.tsx
+++ src/app/threeD/components/threeDView.tsx
@@ -1,24 +1,41 @@
-'use client'
-import { Canvas } from "@react-three/fiber"
+"use client";
+import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
 import { OrbitControls } from "@react-three/drei";
-import Model from "./guitar"
+import Model from "./guitar";
 
+function ThreeDView() {
+    function handleRightClick() {
+        throw new Error("Function not implemented.");
+    }
+    function handleLeftClick() {
+        throw new Error("Function not implemented");
+    }
 
-
-function ThreeDView(){
-    return(
-        <div>
-            <div className="h-[70vh] w-[60vw] border-solid border-zinc-500 border-2">
-                <Canvas camera={{ position: [1, 1, 1] }}>
-                        <ambientLight intensity={Math.PI / 2} />
-                        <Model></Model>
-                        {/* @ts-ignore */}
-                        <OrbitControls autoRotate autoRotateSpeed={0.2} />
-                </Canvas>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="h-[60vh] w-[60vw] border-solid border-zinc-500 border-2">
+        <Canvas camera={{ position: [1, 1, 1] }}>
+          <ambientLight intensity={Math.PI / 2} />
+          <Model></Model>
+          {/* @ts-ignore */}
+          <OrbitControls autoRotate autoRotateSpeed={0.2} />
+        </Canvas>
+        <button
+          onClick={handleRightClick}
+          className="group border-solid border-2 border-zinc-500 hover:border-white rounded-full w-10 h-10 absolute top-2 right"
+        >
+          <div className="relative left-2 border-solid border-zinc-500 group-hover:border-white [border-width:0_3px_3px_0] h-4 w-4 [transform:rotate(-45deg)] [-webkit-transform:rotate(-45deg)]"></div>
+        </button>
+        <button
+        onClick={handleLeftClick}
+        className="group border-solid border-2 border-zinc-500 hover:border-white rounded-full w-10 h-10 absolute top left"
+      >
+        <div className="relative left-3 border-solid border-zinc-500 group-hover:border-white [border-width:0_3px_3px_0] h-4 w-4 [transform:rotate(135deg)] [-webkit-transform:rotate(135deg)]"></div>
+      </button>
+      </div>
+    </div>
+  );
 }
 
-export default ThreeDView
+export default ThreeDView;
--- src/app/threeD/page.tsx
+++ src/app/threeD/page.tsx
@@ -1,17 +1,20 @@
 import ThreeDView from "./components/threeDView";
-import Model from "./components/guitar"
+import Model from "./components/guitar";
 
-
-function ThreeD(){
-    return( 
-        <div className="min-h-screen min-w-full backdrop-blur-sm flex flex-col justify-center items-center font-title font-thin">
-           <div>
-            <ThreeDView></ThreeDView>
-            
-        </div>
-        </div>
-        
-    );
+function ThreeD() {
+  return (
+    <div className="min-h-screen min-w-full backdrop-blur-sm flex flex-col justify-center items-center font-title font-thin">
+      <h1 className="text-6xl w-4/6">3D</h1>
+      <p className="w-4/6 my-6">
+        Besides programming I also do some 3D work. I love to combine the two to
+        make even better experiences. Here I have a couple of models I've worked
+        on.
+      </p>
+      <div className="">
+        <ThreeDView></ThreeDView>
+      </div>
+    </div>
+  );
 }
 
 export default ThreeD;
