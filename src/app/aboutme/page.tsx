ba197042d1550bcd4bc2bf73af76cfc0c8d04b97
MaxRonnow <max.hk.ronnow@gmail.com>
2025-02-04 15:25:13 +0200

    Refactor About Me page content and improve navigation menu styling

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -1,32 +1,33 @@
 function Aboutme() {
   return (
     <div className="backdrop-blur-sm min-w-full min-h-screen flex flex-col items-center font-title font-thin">
-      <div className="max-w-[90vw] mt-20">
-        <h1 className=" text-3xl">A passion for software</h1>
-        <p className="text-lg p-5">
+      <div className="max-w-[90vw] lg:max-w-[60vw] mt-20">
+        <h1 className=" text-3xl my-6">Who am I?</h1>
+        <p className="text-lg my-6">
+          Hi, I'm Max. I am a 21 year old student currently studying computer
+          science. My interest are low level programming, 3D and music. I try to
+          combine my interest in the software and the projects I make.
+        </p>
+        <p className="text-lg my-6">
           My goal with the software that I write is that is solves some kind of
           problem or improves someones life in some way. I aim to create
           performant software that is also beautiful and user friendly.
           Experimentation and learning are the tools I use to create the best
           solutions possible.
         </p>
-
-        <p className="text-lg p-5">
-          I am 21 years old, studying data science at Åbo Akademi University in
-          Turku, Finland.
-        </p>
-        <p className="text-lg p-5">
+        <h2 className="text-2xl my-6">The front page</h2>
+        <p className="text-lg my-6">
           The front page of this portfolio displays a visualization of how
           electrons orbit a hydrogen atom. The shapes of the orbits depend on
           three so called quantum numbers, n, l and m. You can see these numbers
-          displayed in the top right corner on the homepage. The different orbitals represent
-          different energy states of the electron. Per quantum mechanics the
-          electron doesn't have a specific location in space, rather, the
-          density of points represent the probability of finding the elctron
-          there. I find nature to be 
+          displayed in the top right corner on the homepage. The different
+          orbitals represent different energy states of the electron. Per
+          quantum mechanics the electron doesn't have a specific location in
+          space, rather, the density of points represent the probability of
+          finding the elctron there. I find nature to be
         </p>
-        <p className="text-lg p-5">
-          This page is created in NextJS with React Three Fiber for the 3D
+        <p className="text-lg my-6">
+          This website is created in NextJS with React Three Fiber for the 3D
           visuals, Tailwind for styling and Zustand for state management.
         </p>
       </div>
