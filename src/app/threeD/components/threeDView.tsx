f192f56846c64e857f165fb5c9b5bf221fb31882
MaxRonnow <max.hk.ronnow@gmail.com>
2025-02-09 23:30:48 +0200

    Added more 3D models and button functionality to switch between them on 3d page

--- src/app/threeD/components/threeDView.tsx
+++ src/app/threeD/components/threeDView.tsx
@@ -2,18 +2,19 @@
 import { Canvas, Camera } from "@react-three/fiber";
 import * as THREE from "three";
 import { OrbitControls } from "@react-three/drei";
-import Model from "./guitar";
+import Guitar from "./guitar";
+import Chair from "./chair";
 import { useState } from "react";
 
 function ThreeDView() {
-  const model = [0, 1, 2];
-  const currentModel = useState();
+  const models = [<Guitar />, <Chair />];
+  const [currentModel, setModel] = useState(0);
 
   function handleRightClick() {
-    throw new Error("Function not implemented.");
+    setModel((prev) => (prev === models.length - 1 ? 0 : prev + 1));
   }
   function handleLeftClick() {
-    throw new Error("Function not implemented");
+    setModel((prev) => (prev === 0 ? models.length - 1 : prev - 1));
   }
 
   return (
@@ -21,7 +22,7 @@ function ThreeDView() {
       <div className="relative h-[70vh] w-[90vw] lg:w-[60vw] border-solid border-zinc-500 border-2 bg-black">
         <Canvas camera={{ position: [5, 5, 0] }}>
           <ambientLight intensity={Math.PI / 2} />
-          <Model></Model>
+          {models[currentModel]}
           {/* @ts-ignore */}
           <OrbitControls autoRotate autoRotateSpeed={0.2} />
         </Canvas>
@@ -37,6 +38,7 @@ function ThreeDView() {
         >
           <div className="relative left-3 border-solid border-zinc-500 group-hover:border-white [border-width:0_3px_3px_0] h-4 w-4 [transform:rotate(135deg)] [-webkit-transform:rotate(135deg)]"></div>
         </button>
+        <div className="absolute bottom-3 left-[48%] ">{currentModel + 1} / {models.length}</div>
       </div>
     </div>
   );
