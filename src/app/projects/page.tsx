b352642d50a5f174a271e06d9f5fca9f93d06154
MaxRonnow <max.hk.ronnow@gmail.com>
2025-02-10 15:49:07 +0200

    Changed order on projects page and updated README

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,6 +4,67 @@ function Projects() {
   return (
     <div className="h-screen w-full backdrop-blur-sm flex flex-col items-center font-title font-extralight">
       <div className="min-w-10 max-w-[90vw] lg:max-w-[70vw] my-10">
+
+      <div className="my-16">
+          <h1 className="text-3xl py-3 my-3">Portfolio page</h1>
+          <p className="text-lg">
+            This page is created using NextJS and React Three Fiber. It also
+            uses Zustand for state management. The source code is available on
+            Github.
+          </p>
+          <span className="flex flex-row justify-between lg:w-[25%] my-5">
+            <Image
+              src={"./nextjs-fill-svgrepo-com.svg"}
+              height={40}
+              width={40}
+              alt="NextJS logo logo"
+              className="invert"
+            ></Image>
+            <Image
+              src={"./react-svgrepo-com.svg"}
+              height={40}
+              width={40}
+              alt="React logo"
+              className="invert"
+            ></Image>
+            <Image
+              src={"./typescript-svgrepo-com.svg"}
+              width={40}
+              height={40}
+              alt="Typescript logo"
+              className="invert"
+            ></Image>
+            <Image
+              src={"./tailwind.svg"}
+              width={40}
+              height={40}
+              alt="Tailwind logo"
+              className="invert"
+            ></Image>
+            <Image
+              src={"./three.svg"}
+              height={40}
+              width={40}
+              alt="ThreeJS logo"
+              className="invert"
+            ></Image>
+          </span>
+          <a
+            className="group hover:bg-white hover:text-black rounded-full ease-in-out border-solid border-2 border-zinc-500 p-2 my-4 h-auto w-40 flex flex-row justify-evenly"
+            rel="noopener noreferrer"
+            href="https://github.com/"
+          >
+            <Image
+              src={"/github-mark-white.svg"}
+              alt="github logo"
+              width={25}
+              height={20}
+              className="group-hover:invert"
+            ></Image>
+            View on Github
+          </a>
+        </div>
+
         <div className="my-16">
           <h1 className="text-3xl py-3 my-3">Audio effects</h1>
           <p className="text-lg">
@@ -22,7 +83,7 @@ function Projects() {
           <a
             className="group hover:bg-white hover:text-black rounded-full ease-in-out border-solid border-2 border-zinc-500 p-2 my-4 h-auto w-40 flex flex-row justify-evenly"
             rel="noopener noreferrer"
-            href="https://github.com/"
+            href="https://github.com/MaxRonnow/audio_effects"
           >
             <Image
               src={"/github-mark-white.svg"}
@@ -93,65 +154,7 @@ function Projects() {
           </a>
         </div>
 
-        <div className="my-16">
-          <h1 className="text-3xl py-3 my-3">Portfolio page</h1>
-          <p className="text-lg">
-            This page is created using NextJS and React Three Fiber. It also
-            uses Zustand for state management. The source code is available on
-            Github.
-          </p>
-          <span className="flex flex-row justify-between lg:w-[25%] my-5">
-            <Image
-              src={"./nextjs-fill-svgrepo-com.svg"}
-              height={40}
-              width={40}
-              alt="NextJS logo logo"
-              className="invert"
-            ></Image>
-            <Image
-              src={"./react-svgrepo-com.svg"}
-              height={40}
-              width={40}
-              alt="React logo"
-              className="invert"
-            ></Image>
-            <Image
-              src={"./typescript-svgrepo-com.svg"}
-              width={40}
-              height={40}
-              alt="Typescript logo"
-              className="invert"
-            ></Image>
-            <Image
-              src={"./tailwind.svg"}
-              width={40}
-              height={40}
-              alt="Tailwind logo"
-              className="invert"
-            ></Image>
-            <Image
-              src={"./three.svg"}
-              height={40}
-              width={40}
-              alt="ThreeJS logo"
-              className="invert"
-            ></Image>
-          </span>
-          <a
-            className="group hover:bg-white hover:text-black rounded-full ease-in-out border-solid border-2 border-zinc-500 p-2 my-4 h-auto w-40 flex flex-row justify-evenly"
-            rel="noopener noreferrer"
-            href="https://github.com/"
-          >
-            <Image
-              src={"/github-mark-white.svg"}
-              alt="github logo"
-              width={25}
-              height={20}
-              className="group-hover:invert"
-            ></Image>
-            View on Github
-          </a>
-        </div>
+        
       </div>
     </div>
   );
